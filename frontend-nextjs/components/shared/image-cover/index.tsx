43032191f615a4b7077dc520e75b9b84245074a4
Fachri Riyanto <fachririyanto@gmail.com>
2023-12-21 19:09:51 +0700

    Add image cover component

---
+++ frontend-nextjs/components/shared/image-cover/index.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image'
+
+interface ImageCoverProps {
+    w: number
+    h: number
+    src: string
+    alt?: string
+    className?: string
+    fill?: boolean
+    priority?: boolean
+}
+
+export function ImageCover({ alt, w, h, src, className, ...rest }: ImageCoverProps) {
+    const ratio = w / h * 100
+
+    return (
+        <figure className={ `relative overflow-hidden ${className}` } style={{
+            paddingTop: `${ratio}%`,
+        }}>
+            <picture className="flex absolute inset-0">
+                <Image
+                    alt={ alt || '' }
+                    src={ src }
+                    sizes="100%"
+                    { ...rest }
+                />
+            </picture>
+        </figure>
+    )
+}
